2d9bcc07f2a99555a7206a4bc73c939f46fda711
souza_isaa._ . <ipsp3141@gmail.com>
2024-11-17 20:14:46 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +7,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'proj-angular';
+  title: string = 'O que é Angular?';
+  description: string = `
+    Angular é uma plataforma de desenvolvimento de aplicações web front-end mantida pelo Google.
+    Ele permite a criação de Single Page Applications (SPA) com interfaces dinâmicas utilizando TypeScript.
+    O Angular é amplamente utilizado para criar aplicativos escaláveis e de alta performance.
+  `;
+  features: string[] = [
+    'Componentes Reutilizáveis',
+    'Utiliza TypeScript como base',
+    'Suporte a Two-Way Data Binding',
+    'Ferramentas de Teste Integradas',
+    'Injeção de Dependência',
+    'Roteamento Avançado'
+  ];
 }
